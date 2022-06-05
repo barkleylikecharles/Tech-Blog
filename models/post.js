a34595d1dc95d4300f5a16b7fff1b409e6e8c831
Nicole Barkley <barkleylikecharles@icloud.com>
2022-06-05 11:05:55 -0400

    changed DataType of Content and minimal styling of dashboard

--- models/post.js
+++ models/post.js
@@ -41,7 +41,7 @@ Post.init(
         allowNull: false
       },
       post_content: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
       },
       user_id: {
